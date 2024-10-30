1b0a399f8f50c241497d1557437d0c3b8b63ce7d
thilakreddy <thilakreddy19102000@gmail.com>
2024-10-31 01:30:04 +0530

    Fix: all

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           <header className="px-4 lg:px-6 h-14 flex items-center border-b">
             <Link className="flex items-center justify-center" href="/">
               <Briefcase className="h-6 w-6 text-primary" />
-              <span className="ml-2 text-xl font-bold">CareerNexus</span>
+              <span className="ml-2 text-xl font-bold">CarrerNexus</span>
             </Link>
             <nav className="ml-auto md:flex gap-4 sm:gap-6 hidden">
               <Link
@@ -63,7 +63,7 @@ export default function RootLayout({
           <main className="flex-1">{children}</main>
           <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
             <p className="text-xs text-gray-500 dark:text-gray-400">
-              © 2024 CareerNexus Inc. All rights reserved.
+              © 2024 CarrerNexus Inc. All rights reserved.
             </p>
             <nav className="sm:ml-auto flex gap-4 sm:gap-6">
               <Link
